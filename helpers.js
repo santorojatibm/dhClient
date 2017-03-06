eb14a7a6dcdc9b07d39a16a77670807f9283141e
John Santoro <santoroj@us.ibm.com>
2017-03-06 14:37:14 -0500

    trigger change?

--- helpers.js
+++ helpers.js
@@ -6,7 +6,7 @@
 //     Foundation
 //
 // Dream Home Application for node.js 
-// Author: Sal Carceller Jr.
+// Author: Sal Carceller Jr. -> John Santoro
 // Date: 10/28/2016
 //
 // This helpers.js module contains common helper functions for Dream Home App
